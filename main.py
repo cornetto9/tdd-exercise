54770f4b6481f789eb6fa3aa8252aa99973b6f7e
Corntto9 <>
2024-09-11 15:15:57 -0700

    insert message

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand):
-    pass
+    sum = 0
+    for card in hand:
+        if card in VALID_CARDS:
+            if card == 'Jack' or card == 'Queen' or card == 'King': 
+                card = 10
+                sum += card
+            
+            elif card == 'Ace' and sum > 11:
+                card = 1
+                sum += card
+
+            elif card == 'Ace':
+                card = 11
+                sum += card
+
+            else:
+                sum += card
+
+            
+    return sum
+
+
